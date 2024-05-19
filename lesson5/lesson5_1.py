afae33872732c1f90e7ef8e8cc47cdb12a24b5b4
Sean Xie <sean.xie@technexion.com>
2024-05-19 12:10:51 +0800

    use custom exception to reduce the indention

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -2,20 +2,31 @@
 # 若超過 300分則以 300分記
 # 有些學生可以加 5%
 
+class A(Exception):
+	pass
+
 try:
-	scores = int(input("請輸入學生分數(最高300分):"))
+	scores = int(input("請輸入學生分數(0~300):"))
 	if scores > 300 or scores < 0:
 		print("Input value over the range (0~300), please input again.")
-	else:
-		is_add = input("學生是否符合加分條件?(y/n)")
-		if is_add == 'y':
-			scores *= 1.05
-		if scores > 300:
-			scores = 300
+		# add exception message
+		#raise Exception("InputOverRange")
+		# use a custom exception instead of goto of C
+		raise A
 
-		print(f"學生分數是: {round(scores, 0)}")
+	is_add = input("學生是否符合加分條件?(y/n)")
+	if is_add == 'y':
+		scores *= 1.05
+	if scores > 300:
+		scores = 300
+
+	print(f"學生分數是: {round(scores, 0)}")
 except ValueError:
-	print("Invalid value, check your input.")
+	print("ERROR: Invalid value, check your input.")
+except A:
+	print("ERROR: Input over range (0~300) ...")
 except Exception as e:
 	print(f"Exception: {type(e)}, check your input.")
+	print(f"Exception: {e}, check your input.")
+
 print("End this program.")
