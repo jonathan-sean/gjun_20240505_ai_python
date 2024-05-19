1775c54262faf141abe635937c8f8a2ea59365bf
Sean Xie <sean.xie@technexion.com>
2024-05-19 10:32:18 +0800

    add dicition to check the latest score

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -1,9 +1,12 @@
 # 學生的總分最高為 300分
+# 若超過 300分則以 300分記
 # 有些學生可以加 5%
 
 scores = int(input("請輸入學生分數(最高300分):"))
 is_add = input("學生是否符合加分條件?(y/n)")
 if is_add == 'y':
 	scores *= 1.05
+if scores > 300:
+	scores = 300
 
 print(f"學生分數是: {round(scores, 0)}")
