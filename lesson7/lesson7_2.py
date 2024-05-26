aada7a46b528576895edb8fdec22b6db0af85e87
Sean Xie <sean.xie@technexion.com>
2024-05-26 10:57:28 +0800

    modify lesson7_2.py

--- lesson7/lesson7_2.py
+++ lesson7/lesson7_2.py
@@ -2,19 +2,24 @@ import random
 import pyinputplus as pyip
 
 min:int = 1
-max:int = 10
+max:int = 100
+count:int = 0
+hint:bool = False	# let user guess easier
 
 target = random.randint(min, max)
 print("----------- Guessing Number Game -----------")
 while(True):
-	keyin = pyip.inputInt(f"Input your number ({min}~{max}): ", min=min, max=max)
+	count += 1
+	keyin = pyip.inputInt(f"Guess#{count}: Input your number ({min}~{max}): ", min=min, max=max)
 	if(keyin > target):
 		print("Too large ...")
+		if hint: max = keyin - 1
 		continue
 	if(keyin < target):
 		print("Too small ...")
+		if hint: min = keyin + 1
 		continue
 	print("You win!")
 	break
-
-print("Game Over")
+print(f"You guess {count} times")
+print("Thanks for your playing.")
