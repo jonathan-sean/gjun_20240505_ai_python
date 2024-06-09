49676d332256874f70bb0bd787e0655525dc2719
Sean Xie <sean.xie@technexion.com>
2024-06-09 09:52:36 +0800

    add type hint

--- lesson10/homework_2/lesson10_homework2.py
+++ lesson10/homework_2/lesson10_homework2.py
@@ -1,20 +1,20 @@
-import csv
+from csv import DictReader
 import pyinputplus as pyip
 from pprint import pprint
 from random import choices
 
 def main():
-	fname = "個股日成交資訊.csv"
+	fname:str = "個股日成交資訊.csv"
 	with open(fname, encoding="UTF-8", newline='') as f:
-		reader = csv.DictReader(f)
-		lst = list(reader)
+		reader:DictReader = DictReader(f)
+		lst:list[dict] = list(reader)
 		print(str.format("{}共有 {} 筆資料", fname.removesuffix(".csv"), len(lst)))
 		# Too much data, just show some to demonstration
 		n = pyip.inputInt("請輸入要隨機顯示的資料數(1~20): ", min=1, max=20)
-		lst_2 = choices(lst, k=n)
+		lst_2:list[dict] = choices(lst, k=n)
 		for d in lst_2:
 			# Get index of complete data
-			idx = lst.index(d)
+			idx:int = lst.index(d)
 			print(f"#{idx}")
 			pprint(d, indent=2, sort_dicts=False)
 '''
